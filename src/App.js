f1853de162e98d4039a70e45bf0daee6dcb1aa43
Jason Davis <jasondavis3@gmail.com>
2021-09-23 14:56:48 -0700

    init repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from "react-router-dom";
+import {Home, 
+  Store, 
+  Signup,
+  Signout,
+  Cart,
+  Faq,
+  Contact} from "./pages"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/store" element={<Store />}/>
+        <Route path="/signup" element={<Signup />}/>
+        <Route path="/signout" element={<Signout />}/>
+        <Route path="/cart" element={<Cart />}/>
+        <Route path="/faq" element={<Faq />}/>
+        <Route path="/contact" element={<Contact />}/>
+      </Routes>
+      <section>
+        
+      </section>
     </div>
   );
 }
---
+++ src/pages.js
@@ -0,0 +1,51 @@
+import React from "react"
+
+export function Home(){
+	return(
+		<div>
+			<h1>Home</h1>
+		</div>
+	);
+}
+export function Store(){
+	return(
+		<div>
+			<h1>Store</h1>
+		</div>
+	);
+}
+export function Signup(){
+	return(
+		<div>
+			<h1>Signup</h1>
+		</div>
+	);
+}
+export function Signout(){
+	return(
+		<div>
+			<h1>Signout</h1>
+		</div>
+	);
+}
+export function Cart(){
+	return(
+		<div>
+			<h1>Cart</h1>
+		</div>
+	);
+}
+export function Contact(){
+	return(
+		<div>
+			<h1>Contact</h1>
+		</div>
+	);
+}
+export function Faq(){
+	return(
+		<div>
+			<h1>FAQ</h1>
+		</div>
+	);
+}
